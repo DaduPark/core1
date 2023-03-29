0e03b8aa2ea6910167287b2cf52c5b87d0636248
DaduPark <koreatkdtn@gmail.com>
2023-03-29 23:28:44 +0900

    Appconfig(DI역할 class) 리펙토링

--- src/main/java/hello/core1/AppConfig.java
+++ src/main/java/hello/core1/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core1;
 
+import hello.core1.discount.DiscountPolicy;
 import hello.core1.discount.FixDiscountPolicy;
 import hello.core1.member.MemberService;
 import hello.core1.member.MemberServiceImpl;
@@ -10,10 +11,19 @@ import hello.core1.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
 }
