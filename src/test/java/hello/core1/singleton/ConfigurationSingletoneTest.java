914fdfe9315abdb2cb590429fac974c54d266aa5
DaduPark <koreatkdtn@gmail.com>
2023-04-04 23:41:05 +0900

    componentScan 작성

--- src/test/java/hello/core1/singleton/ConfigurationSingletoneTest.java
+++ src/test/java/hello/core1/singleton/ConfigurationSingletoneTest.java
@@ -29,4 +29,12 @@ public class ConfigurationSingletoneTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configrationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+    }
 }
