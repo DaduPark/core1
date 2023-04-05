20c844190594a972232faf6df5f848d24d9aa882
DaduPark <koreatkdtn@gmail.com>
2023-04-06 07:19:12 +0900

    생성자 주입 변경

---
+++ src/test/java/hello/core1/order/OrderServiceImplTest.java
@@ -0,0 +1,25 @@
+package hello.core1.order;
+
+import hello.core1.discount.FixDiscountPolicy;
+import hello.core1.member.Grade;
+import hello.core1.member.Member;
+import hello.core1.member.MemberRepository;
+import hello.core1.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        MemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
+    }
+
+}
