c32c5fd9f36ce9a5402fb42cf9ed560d1e983c56
DaduPark <koreatkdtn@gmail.com>
2023-04-05 21:27:31 +0900

    빈 충돌 상황 등록

--- src/main/java/hello/core1/AutoAppConfig.java
+++ src/main/java/hello/core1/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core1;
 
+import hello.core1.member.MemberRepository;
+import hello.core1.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)//기존예제코드들 예외시키기위해
 )
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 
 }
